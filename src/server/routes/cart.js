71d047c57ec81dabc74a5731566c763d5a76649f
SimeonKauffmann <simeon.kauffmann@gmail.com>
2021-11-18 09:00:00 +0100

    added get post and put to cart route

--- src/server/routes/cart.js
+++ src/server/routes/cart.js
@@ -1,8 +1,42 @@
 const express = require("express")
-const router = express.router()
+const router = express.Router()
 
-router.get("/", (req, res) => {
-  res.send()
+const data = require("../../db/dbDriver").cartsData
+
+router.get("/:userLogin", (req, res) => {
+  const order = data.find(
+    (cart) => cart.user.userLogin === req.params.userLogin
+  )
+  res.send(order.cart)
+})
+
+router.post("/:userLogin", (req, res) => {
+  const newOrder = req.body
+
+  data.push({ user: { userLogin: req.params.userLogin }, cart: newOrder })
+
+  res
+    .status(201)
+    .send(data.find((cart) => cart.user.userLogin === req.params.userLogin))
+})
+
+router.put("/:userLogin/:productId", (req, res) => {
+  data.forEach((cart) => {
+    if (cart.user.userLogin === req.params.userLogin) {
+      if (
+        cart.cart.find((product) => {
+          product.productId === req.params.productId
+        })
+      ) {
+        cart.cart.indexOf(product.productId === req.params.productId).product
+          .amount++
+      } else {
+        cart.cart.push({ productId: req.params.productId, amount: 1 })
+      }
+    }
+  })
+
+  res.send(data.find((cart) => cart.user.userLogin === req.params.userLogin))
 })
 
 module.exports = router
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,13 @@
-const cartRouter = require("routes/cart")
-const userRouter = require("routes/user")
-const productsRouter = require("routes/products")
+const cartRouter = require("./routes/cart")
+const userRouter = require("./routes/user")
+const productsRouter = require("./routes/products")
 
 const express = require("express")
 const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.use("/cart", cartRouter)
+app.use("/carts", cartRouter)
 
 app.get("/", (req, res) => {
   res.send()

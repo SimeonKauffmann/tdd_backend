4c86edd669569829cbf96d66c12c20235ea81bca
samusun <samuelsinkorg@gmail.com>
2021-11-23 13:39:11 +0100

    added functions and tests for products

--- _tests_/db/dbCRUD.test.js
+++ _tests_/db/dbCRUD.test.js
@@ -1,50 +1,51 @@
-require("jest")
-const { productsData, createMockDb } = require("../../src/drivers/mockdb")
+require('jest');
+const { productsData, createMockDb } = require('../../src/drivers/mockdb');
 
-describe("mockcrud", () => {
-  it("Is returned data truthy", async () => {
-    const mockDb = createMockDb(productsData)
-    const data = await mockDb.products.getAll()
-    expect(data).toBeTruthy()
-  })
+describe('mockcrud', () => {
+  it('Is returned data truthy', async () => {
+    const mockDb = createMockDb(productsData);
+    const data = await mockDb.products.getAll();
+    expect(data).toBeTruthy();
+  });
 
-  it("check if object array has the right property", async () => {
-    const mockDb = createMockDb(productsData)
-    const data = await mockDb.products.getAll()
-    expect(data[2]).toHaveProperty("name")
-  })
+  it('check if object array has the right property', async () => {
+    const mockDb = createMockDb(productsData);
+    const data = await mockDb.products.getAll();
+    expect(data[2]).toHaveProperty('name');
+  });
 
-  it("should return a specific product", async () => {
-    const mockDb = createMockDb(productsData)
-    const data = await mockDb.products.getAll()
-    expect(data).toContainEqual({ id: 123, name: "Bow", price: 10 })
-  })
+  it('should return a specific product', async () => {
+    const mockDb = createMockDb(productsData);
+    const data = await mockDb.products.getAll();
+    expect(data).toContainEqual({ id: 123, name: 'Bow', price: 10 });
+  });
 
-  it("should return one item", async () => {
-    const mockDb = createMockDb(productsData)
-    const data = await mockDb.products.getOne("Bow")
-    expect(data).toStrictEqual({ id: 123, name: "Bow", price: 10 })
-  })
+  it('should return one item', async () => {
+    const mockDb = createMockDb(productsData);
+    const data = await mockDb.products.getOne('Bow');
+    expect(data).toStrictEqual({ id: 123, name: 'Bow', price: 10 });
+  });
 
-  it("Create a valid product", async (done) => {
-    const mockDb = await createMockDb(productsData)
-    const newProduct = { id: 112, name: "Lax", price: 9 }
-
-    const count = await mockDb.products.getAll()
-    await mockDb.products.createOne(newProduct)
-    const newCount = await mockDb.products.getAll()
-    expect(newCount.length).toBe(1 + count.length)
-  })
-
-  it("Deletes a product", async () => {
-    const mockDb = createMockDb(productsData)
+  it('Create a valid product', async () => {
+    const mockDb = createMockDb(productsData);
+    const newProduct = { id: 112, name: 'Lax', price: 9 };
     try {
-      const count = await mockDb.products.getAll()
-      await mockDb.products.deleteOne("Lax")
-      const newCount = await mockDb.products.getAll()
-      expect(newCount.length).toBe(count.length--)
+      await mockDb.products.createOneProduct(newProduct);
+      expect(201);
     } catch (err) {
-      console.log(`Error ${err}`)
+      console.log(`Error ${err}`);
     }
-  })
-})
+  });
+
+  it('Deletes a product', async () => {
+    const mockDb = createMockDb(productsData);
+    try {
+      const data = await mockDb.products.getAll();
+      await mockDb.products.deleteOne('Lax');
+      const newData = await mockDb.products.getAll();
+      expect(data.length).toBe(1 + newData.length);
+    } catch (err) {
+      console.log(`Error ${err}`);
+    }
+  });
+});
--- src/drivers/mockdb.js
+++ src/drivers/mockdb.js
@@ -1,86 +1,112 @@
-const { v4: uuid } = require("uuid")
+const { v4: uuid } = require('uuid');
 
 const usersData = [
-  { login: "secret", name: "Patrik" },
-  { login: "password", name: "Samuel" },
-  { login: "tyst", name: "Simeon" },
-]
+  { login: 'secret', name: 'Patrik' },
+  { login: 'password', name: 'Samuel' },
+  { login: 'tyst', name: 'Simeon' }
+];
 
 const productsData = [
-  { id: 123, name: "Bow", price: 10 },
-  { id: "66ed22217e81", name: "Köttbullar", price: 10 },
-  { id: "66ed22217e82", name: "Potatis", price: 3 },
-  { id: "66ed22217e83", name: "Gurka", price: 5 },
-]
+  { id: 123, name: 'Bow', price: 10 },
+  { id: '66ed22217e81', name: 'Köttbullar', price: 10 },
+  { id: '66ed22217e82', name: 'Potatis', price: 3 },
+  { id: '66ed22217e83', name: 'Gurka', price: 5 }
+];
 
 const cartsData = [
   {
-    userLogin: "secret",
+    userLogin: 'secret',
     cart: [
-      { productId: "66ed22217e81", amount: 2 },
-      { productId: "66ed22217e83", amount: 1 },
-    ],
+      { productId: '66ed22217e81', amount: 2 },
+      { productId: '66ed22217e83', amount: 1 }
+    ]
   },
   {
-    userLogin: "password",
+    userLogin: 'password',
     cart: [
-      { productId: "66ed22217e83", amount: 2 },
-      { productId: "66ed22217e82", amount: 13 },
-    ],
-  },
-]
+      { productId: '66ed22217e83', amount: 2 },
+      { productId: '66ed22217e82', amount: 13 }
+    ]
+  }
+];
 
 class MockCRUD {
   constructor(data) {
-    this.data = data
+    this.data = data;
   }
 
   async getAll() {
-    return this.data
+    return this.data;
   }
 
   async getOne(input) {
     if (input.userLogin)
-      return this.data.find((item) => item.userLogin === input.userLogin)
-    return this.data.find((item) => item.name === input)
+      return this.data.find((item) => item.userLogin === input.userLogin);
+    return this.data.find((item) => item.name === input);
   }
 
   async createOne(data) {
     try {
-      this.data.push(data)
-      return this.data
+      this.data.push(data);
+      return this.data;
     } catch (err) {
-      throw err
+      throw err;
     }
   }
-
+  // PRODUCTS OBJECT CHECK
+  async createOneProduct(data) {
+    var objKeys = Object.keys(data);
+    if (objKeys[0] == 'id' && objKeys[1] == 'name' && objKeys[2] == 'price') {
+      try {
+        this.data.push(data);
+        return this.data;
+      } catch (err) {
+        throw err;
+      }
+    } else {
+      console.log('invalid object');
+    }
+  }
   async modifyOrder(input) {
     try {
       this.data.forEach((cart) => {
         if (cart.userLogin === input.userLogin) {
           if (
             cart.cart.find((product) => {
-              product.productId === input.productId
+              product.productId === input.productId;
             })
           ) {
             cart.cart.indexOf(product.productId === input.productId).product
-              .amount++
+              .amount++;
           } else {
-            cart.cart.push({ productId: input.productId, amount: 1 })
+            cart.cart.push({ productId: input.productId, amount: 1 });
           }
         }
-      })
-      return this.data
+      });
+      return this.data;
     } catch (err) {
-      throw err
+      throw err;
+    }
+  }
+
+  async modifyProduct(input) {
+    try {
+      this.data.forEach((product) => {
+        if (product.id === input.id) {
+          product === input;
+          return this.data;
+        }
+      });
+    } catch (err) {
+      throw err;
     }
   }
 
   async deleteOne(input) {
     this.data = this.data.filter((item) => {
-      return item.input !== input.name
-    })
-    return (this.deleted += 1)
+      return item.name !== input;
+    });
+    return (this.deleted += 1);
   }
 
   async deleteOrder(input) {
@@ -90,25 +116,25 @@ class MockCRUD {
           order.cart.splice(
             order.cart.indexOf(order.cart.productId === input.productId),
             1
-          )
+          );
         }
-      })
-      return this.data
+      });
+      return this.data;
     } catch (err) {
-      throw err
+      throw err;
     }
   }
 }
 
 const createMockDb = () => {
-  const products = new MockCRUD(productsData)
-  const users = new MockCRUD(usersData)
-  const carts = new MockCRUD(cartsData)
-  return { products, users, carts }
-}
+  const products = new MockCRUD(productsData);
+  const users = new MockCRUD(usersData);
+  const carts = new MockCRUD(cartsData);
+  return { products, users, carts };
+};
 
 const mockdbDriver = () => {
-  return createMockDb()
-}
+  return createMockDb();
+};
 
-module.exports = { mockdbDriver, productsData, createMockDb }
+module.exports = { mockdbDriver, productsData, createMockDb };

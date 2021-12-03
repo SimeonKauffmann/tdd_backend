d09df1d7a4d83c2fd6eae0fc28ec93bcb65b31b6
SimeonKauffmann <simeon.kauffmann@gmail.com>
2021-12-03 12:00:17 +0100

    added npm run start

---
+++ app.js
@@ -0,0 +1,5 @@
+const app = require("./src/server/server")
+
+app.listen(3001, () => {
+  console.log("app listening at localhost:3001")
+})
--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,12 @@ const cartRouter = require("./routes/cart.js")
 const userRouter = require("./routes/user.js")
 const productsRouter = require("./routes/products.js")
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 app.use("/products", productsRouter)
 app.use("/carts", cartRouter)
